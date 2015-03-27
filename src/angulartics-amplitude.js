ca3d96c0f494768da0007e7e4b88ad119491a74a
Martijn de Jongh <martijn.de.jongh@woodwing.com>
2015-03-27 15:41:23 +0100

    Changed required for pro client

---
+++ src/angulartics-amplitude.js
@@ -0,0 +1,29 @@
+/**
+ * @license Angulartics v0.17.2
+ * (c) 2013 Luis Farzati http://luisfarzati.github.io/angulartics
+ * Contributed by http://github.com/martijndejongh
+ * License: MIT
+ */
+
+(function(angular) {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name angulartics.amplitude
+   * Enables analytics support for amplitude (http://amplitude.com)
+   */
+  angular.module('angulartics.amplitude', ['angulartics'])
+    .config(['$analyticsProvider', function ($analyticsProvider) {
+
+
+      $analyticsProvider.registerPageTrack(function (path) {
+        //No separate track page functionality
+      });
+
+      $analyticsProvider.registerEventTrack(function (action, properties) {
+        amplitude.logEvent(action, properties);
+      });
+
+    }]);
+})(angular);
